1308a50622e95b9a1117683495d53ca490aa6c2a
kimurarie <g031t305@s.iwate-pu.ac.jp>
2022-04-16 23:37:01 +0900

    ログイン機能の完成

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,29 +1,79 @@
 import { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { equalTo, getDatabase, orderByChild, query, ref, get, child, set } from 'firebase/database';
+import { Redirect } from 'react-router-dom';
 
 
 const Login = () => {
 
-  const [info, setInfo] = useState('');
+  const [page, setPage] = useState('login');
+  const [jump, setJump] = useState(false);
+  const [nickname, setName] = useState('');
+  const [uid, setId] = useState('');
 
-  const get = () => {
+  useEffect(async () => {
+    const data = await getInfo();
+    const id =  data.uid;
+    const name = await getDb(id);
+    setId(id);
+    if (name === '') {
+      setPage('singup');
+    } else {
+      setJump(true);
+    }
+
+  }, [])
+
+  const getInfo = () => {
     return new Promise(resolve => {
       const auth = getAuth();
       onAuthStateChanged(auth, (user) => {
-        resolve(user);        
+        resolve(user);
       });
     })
   }
 
-  useEffect(async() => {
-    // console.log((await get()));
-  }, [])
+  const getDb = (id) => {
+    return new Promise(resolve => {
+      const deRef = ref(getDatabase());
+      get(child(deRef, '/user/' + id)).then((snapshot) => {
 
-  return (
-    <div>
-      ログイン
-    </div>
-  );
+        const result = snapshot.val();
+        if (result === null)
+          resolve('');
+        else
+          resolve(result.name);
+      })
+    })
+  }
+
+  const signUp = () => {
+    const dbRef = ref(getDatabase(),'/user/'+uid);
+    set(dbRef,{
+      name:nickname
+    })
+    setJump(true);
+  }
+
+  if (jump) {
+    return <Redirect to="/home" />
+  }
+
+  if (page === 'login') {
+    return (
+      <div>
+        loading...
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        新規登録
+        <input type="text" value={nickname} onChange={(e) => setName(e.target.value)}></input>
+        <button onClick={() => signUp()}>新規登録</button>
+      </div>
+    );
+  }
 }
 
 export default Login; // 外部から呼び出せるようにする
