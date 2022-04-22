a3531e0d79f890c18eee1597fe8c0135a7393ac8
kimurarie <g031t305@s.iwate-pu.ac.jp>
2022-04-22 18:04:40 +0900

    ログアウト機能の実装

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
 import { database } from '../FirebaseConfig.js';
 import { onValue, ref, push} from 'firebase/database';
+import { getAuth, signOut } from 'firebase/auth';
+import { Redirect } from 'react-router';
 
 
 const Home = () => {
 
   const [text, setText] = useState('');
   const [list, setList] = useState('');
+  const [jump, setJump] = useState(false);
 
   useEffect(() => { // 無限ループ対策
     onValue(ref(database, 'posts'), (snapshop) => {
@@ -26,11 +29,25 @@ const Home = () => {
     setText('');
   }
 
+  // ログアウト処理
+  const logout = () => {
+    const auth = getAuth();
+    signOut(auth).then( () => {
+      setJump(true);
+      console.log(jump)
+    });
+  }
+
+  if (jump) {
+    return <Redirect to="/" />
+  }
+
   return (
     <div>
       {list}
       <input type="text" value={text} onChange={(e) => setText(e.target.value)}></input>
       <button onClick={() => post()}>投稿</button>
+      <button onClick={() => logout()}>ログアウト</button>
     </div>
   );
 }
